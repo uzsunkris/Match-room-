5ac9dba681f9643982f80cb6c0dc7d70609b9692
uzsunkris <84834511+uzsunkris@users.noreply.github.com>
2026-03-05 14:10:07 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBXzeF1LsdQxg9RcZ3KyMzWimBXKwBAyaw",
+    authDomain: "match-room-a469b.firebaseapp.com",
+    projectId: "match-room-a469b",
+    storageBucket: "match-room-a469b.firebasestorage.app",
+    messagingSenderId: "621908983860",
+    appId: "1:621908983860:web:bb5b683d1322683ee34a61"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
