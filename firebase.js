8f4deea6d3f39606647e2275aaf560beee9b14a7
uzsunkris <84834511+uzsunkris@users.noreply.github.com>
2026-03-09 16:24:10 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -17,11 +17,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app); // Export auth for use in auth.js
 
 
-import { auth } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("Your UID:", user.uid);
-  }
-});
